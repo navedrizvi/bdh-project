a46bb98a7f8e293ae7177125281adfb02f8526bf
Naved <rizvi.naved1997@gmail.com>
2021-12-12 12:12:29 -0500

    fix sample size for single machine

--- src/preprocessing-py.py
+++ src/preprocessing-py.py
@@ -27,7 +27,7 @@ PATH_PROCESSED = '../data/processed/'
 DIAG_PATH = RAW_BASE_PATH.format(fname=DIAGNOSES_FNAME)
 PATIENTS_PATH = RAW_BASE_PATH.format(fname=PATIENTS_FNAME)
 
-PATIENT_SAMPLE_SIZE = 46520 # total is 46520
+PATIENT_SAMPLE_SIZE = 1000 # total is 46520
 TRAIN_SIZE = 0.8
 # We need to take into account only the events that happened during the observation window. The end of observation window is N days before death for deceased patients and date of last event for alive patients. We can have several sets of events (e.g. labs, diags, meds), so we need to choose the latest date out of those.
 # OBSERVATION_WINDOW = 2000
